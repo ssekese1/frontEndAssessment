ac278ee88adf54ad9bef1a86855dfce13ccf619f
Senaki Sekese <senaki.sekese@moov.life>
2021-08-18 12:36:48 +0200

    final product

--- pages/js/randomUser.js
+++ pages/js/randomUser.js
@@ -1,10 +1,4 @@
-// const refresh = document.querySelector(".refresh");
-// refresh.addEventListener("click", function () {
-//     location.reload();
-// })
-
-
-const title_details_ = document.querySelector(".title_details");
+const user_details_ = document.querySelector(".user_details");
 const info_icons = document.querySelector(".info_icons");
 const user_img = document.querySelector(".user_img");
 
@@ -13,38 +7,37 @@ fetch('https://randomuser.me/api/')
     .then(response => response.json())
     .then(function (data) {
         let userData = data.results[0];
-        console.log(userData);
         let imgSrc = `<img src="${userData.picture.large}">`;
-        let location = `${userData.location.street.number} ${userData.location.street.name}`;
-        let name_ = `${userData.name.first} ${userData.name.last}`;
+        let user_name = `${userData.name.first} ${userData.name.last}`;
         var date = new Date(userData.dob.date);
-        var birthDate = date.getMonth() + 1 + "/" + (date.getDay() + 1) + "/19" + date.getYear();
+        var birthDate = date.getMonth() + 1 + '/' + (date.getDay() + 1) + '/' +  date.getFullYear();
+        let location = `${userData.location.street.number} ${userData.location.street.name}`;
 
-        let content_ = `
-        <div class="icon user_ active" data-title="Hi, My name is" data-value="${name_}"></div>
+        let userContent = `
+        <div class="icon user_ active" data-title="Hi, My name is" data-value="${user_name}"></div>
         <div class="icon email_" data-title="My email address is" data-value="${userData.email}"></div>
         <div class="icon birthday_" data-title="My birthday is" data-value="${birthDate}"></div>
         <div class="icon location_" data-title="My addres is" data-value="${location}"></div>
         <div class="icon phone_" data-title="My phone number is " data-value="${userData.cell}"></div>
         <div class="icon password_" data-title="My password is" data-value="${userData.login.password}"></div>        
         `
-        info_icons.innerHTML = content_;
+        info_icons.innerHTML = userContent;
         user_img.innerHTML = imgSrc;
-        title_details_.innerHTML = name_;
+        user_details_.innerHTML = user_name;
 
 
         //for hover and change details
         const icon = document.querySelectorAll(".icon");
-        const title_info = document.querySelector(".title_info");
-        const title_details = document.querySelector(".title_details");
+        const user_info = document.querySelector(".user_info");
+        const user_details = document.querySelector(".user_details");
 
         icon.forEach(function (item) {
             item.addEventListener("mouseover", function () {
-                let titleData = item.getAttribute("data-title");
+                let dataTitle = item.getAttribute("data-title");
                 let dataValue = item.getAttribute("data-value");
 
-                title_info.innerHTML = titleData;
-                title_details.innerHTML = dataValue;
+                user_info.innerHTML = dataTitle;
+                user_details.innerHTML = dataValue;
 
                 let activeClass = document.querySelectorAll(".active");
 
